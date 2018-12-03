1a3fbff1e11dd81810093fe8fe5bad20d4fa57f0
AUzhegov <test>
2018-12-03 13:26:33 +0300

    Add CategoryService

---
+++ app/Services/CategoryService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Category;
+use App\Repositories\Category\CategoryRepository;
+
+class CategoryService
+{
+    /**
+     * @var Category
+     */
+    private $categoryModel;
+    /**
+     * @var CategoryRepository
+     */
+    public $repository;
+
+
+    /**
+     * ProductService constructor.
+     * @param Category $categoryModel
+     * @param CategoryRepository $categoryRepository
+     */
+    public function __construct(Category $categoryModel, CategoryRepository $categoryRepository)
+    {
+        $this->categoryModel = $categoryModel;
+        $this->repository = $categoryRepository;
+    }
+
+    /**
+     * Adds new product with relations
+     * @param array $attributes
+     * @return bool
+     */
+    public function add(array $attributes): bool
+    {
+        $createdCategory = $this->categoryModel->fill($attributes);
+        $createdCategory->save();
+
+        if(isset($attributes['image'])) {
+            $createdCategory->addSingleImage($attributes['image']);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param int $id
+     * @param array $attributes
+     * @return bool
+     */
+    public function update(int $id, array $attributes): bool
+    {
+        /** @var Category $updatedCategory */
+        $updatedCategory = $this->categoryModel->find($id);
+        $updatedCategory->fill($attributes);
+        $updatedCategory->save();
+
+        if($attributes['image']) {
+            $updatedCategory->addSingleImage($attributes['image']);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param int $id
+     * @return bool
+     */
+    public function destroy(int $id): bool
+    {
+        $categoryToDelete = $this->categoryModel->findOrFail($id);
+        $categoryToDelete->delete();
+        $categoryToDelete->categories()->detach($id);
+
+        return true;
+    }
+}
