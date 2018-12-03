2ad2f3871678fcd634ba22c934629d00333c98a7
Андрей Ужегов <AUzhegov@7x.kazanworkout.ru>
2018-12-03 12:53:33 +0300

    Complete EloquentCategoryRepository

--- app/Repositories/Category/EloquentCategoryRepository.php
+++ app/Repositories/Category/EloquentCategoryRepository.php
@@ -2,8 +2,41 @@
 
 namespace App\Repositories\Category;
 
+use App\Models\Category;
+use App\Repositories\AbstractRepository;
 
-class EloquentCategoryRepository
+class EloquentCategoryRepository extends AbstractRepository implements ProductRepository
 {
+    /**
+     * @var Category
+     */
+    private $model;
 
+    /**
+     * EloquentProductRepository constructor.
+     * @param Category $category
+     */
+    public function __construct(Category $category)
+    {
+        $this->model = $category;
+    }
+
+    /**
+     * @param array $relations
+     * @return mixed
+     */
+    public function getAll($relations = [])
+    {
+        return $this->model->with($relations)->all();
+    }
+
+    /**
+     * @param array $relations
+     * @param int $perPage
+     * @return mixed
+     */
+    public function getPaginated($relations = [], $perPage = 15)
+    {
+        return $this->model->with($relations)->paginate($perPage);
+    }
 }
