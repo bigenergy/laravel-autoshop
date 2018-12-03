4dd687fed544b5eaa70deab58e9968ce710ff573
AUzhegov <test>
2018-12-03 13:02:20 +0300

    Complete EloquentBrandRepository

--- app/Repositories/Brand/EloquentBrandRepository.php
+++ app/Repositories/Brand/EloquentBrandRepository.php
@@ -1,15 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: AUzhegov
- * Date: 03.12.2018
- * Time: 12:57
- */
 
 namespace App\Repositories\Brand;
 
+use App\Models\Brand;
+use App\Repositories\AbstractRepository;
 
-class EloquentBrandRepository
+class EloquentBrandRepository extends AbstractRepository implements BrandRepository
 {
+    /**
+     * @var Brand
+     */
+    private $model;
 
+    /**
+     * EloquentProductRepository constructor.
+     * @param Brand $brand
+     */
+    public function __construct(Brand $brand)
+    {
+        $this->model = $brand;
+    }
+
+    /**
+     * @param array $relations
+     * @return mixed
+     */
+    public function getAll($relations = [])
+    {
+        return $this->model->with($relations)->all();
+    }
+
+    /**
+     * @param array $relations
+     * @param int $perPage
+     * @return mixed
+     */
+    public function getPaginated($relations = [], $perPage = 15)
+    {
+        return $this->model->with($relations)->paginate($perPage);
+    }
 }
