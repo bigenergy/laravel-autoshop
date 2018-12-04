7469a0f6876309f9052967e1a31d247ac9e819b2
AUzhegov <test>
2018-12-04 16:32:08 +0300

    Cleanup MainController

--- app/Http/Controllers/Shop/MainController.php
+++ app/Http/Controllers/Shop/MainController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers\Shop;
 use App\Http\Controllers\Controller;
 use App\Services\CategoryService;
 use App\Services\ProductService;
-use Illuminate\Http\Request;
-use Illuminate\View\View;
+
 
 class MainController extends Controller
 {
@@ -22,19 +21,6 @@ class MainController extends Controller
 
     public function index() {
 
-        $categoryShowList = $this->categoryService->repository->getPaginated();
-        $products = $this->productService->repository->getPaginated(['categories', 'brand']);
-
-        return view('shop.main', [
-            'categoryShowList' => $categoryShowList,
-            'products' => $products
-        ]);
-    }
-
-    public function show($id) {
-        $categoryShowList = $this->categoryService->repository->getPaginated();
-        $products = $this->categoryService->repository->getById($id);
-
-        return view('shop.category.products', ['products' => $products, 'categoryShowList' => $categoryShowList]);
+        return view('shop.main', []);
     }
 }
