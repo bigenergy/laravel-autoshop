0035c9a36615047299cd02aa45eac65eab2eb89e
AUzhegov <test>
2018-12-04 18:10:14 +0300

    Init

--- resources/views/shop/layouts/brands.blade.php
+++
@@ -1,8 +0,0 @@
-<h6>Бренды</h6>
-<nav class="nav nav-pills nav-fill">
-    <a class="nav-item nav-link active" href="#">Active</a>
-    <a class="nav-item nav-link" href="#">Link</a>
-    <a class="nav-item nav-link" href="#">Link</a>
-    <a class="nav-item nav-link disabled" href="#">Disabled</a>
-</nav>
-<br>
