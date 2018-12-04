95f1fd3388a6a1fd290a8af3811bc531b0cc9951
AUzhegov <test>
2018-12-04 16:38:47 +0300

    Init brands for view

---
+++ resources/views/shop/layouts/brands.blade.php
@@ -0,0 +1,8 @@
+<h6>Бренды</h6>
+<nav class="nav nav-pills nav-fill">
+    <a class="nav-item nav-link active" href="#">Active</a>
+    <a class="nav-item nav-link" href="#">Link</a>
+    <a class="nav-item nav-link" href="#">Link</a>
+    <a class="nav-item nav-link disabled" href="#">Disabled</a>
+</nav>
+<br>
