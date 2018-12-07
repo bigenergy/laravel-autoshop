d0307ca545c27b3ea9d7152752d38ad2ece04211
AUzhegov <test>
2018-12-07 13:08:28 +0300

    Fix carts migration

--- database/migrations/2018_12_05_090656_create_carts_table.php
+++ database/migrations/2018_12_05_090656_create_carts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCartTable extends Migration
+class CreateCartsTable extends Migration
 {
     /**
      * Run the migrations.
