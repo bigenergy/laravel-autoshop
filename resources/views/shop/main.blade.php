74e1b44d34cc5c141fb9ee3464313dc06a3088cd
AUzhegov <test>
2018-12-10 15:56:53 +0300

    Added favicon

--- resources/views/shop/main.blade.php
+++ resources/views/shop/main.blade.php
@@ -6,8 +6,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="AUzhegov">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
     <title>AutoShop - Каталог</title>
 
