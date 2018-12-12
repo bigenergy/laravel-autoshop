c9fe08cee55895ecb01386ac3575f272816ad682
AUzhegov <test>
2018-12-12 17:25:14 +0300

    text -> number

--- resources/views/admin/status/form.blade.php
+++ resources/views/admin/status/form.blade.php
@@ -8,7 +8,7 @@
     <div class="form-group">
         {{ Form::label('sort', 'Сортировка', ['class' => 'col-sm-2 control-label']) }}
         <div class="col-sm-10">
-            {{ Form::text('sort', null, ['class' => 'form-control', 'required']) }}
+            {{ Form::number('sort', null, ['class' => 'form-control', 'required']) }}
         </div>
     </div>
     <div class="form-group">
