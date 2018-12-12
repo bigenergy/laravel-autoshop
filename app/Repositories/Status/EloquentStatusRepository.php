40bf363a0589aa86dd35b188e1dba6121bc036f2
AUzhegov <test>
2018-12-12 14:24:17 +0300

    Create status repo

---
+++ app/Repositories/Status/EloquentStatusRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories\Status;
+
+use App\Models\Status;
+use App\Repositories\AbstractRepository;
+
+class EloquentStatusRepository extends AbstractRepository implements StatusRepository
+{
+    /**
+     * @var Status
+     */
+    private $model;
+
+    /**
+     * EloquentProductRepository constructor.
+     * @param Status $status
+     */
+    public function __construct(Status $status)
+    {
+        $this->model = $status;
+        parent::__construct($status);
+    }
+
+    /**
+     * @param array $relations
+     * @return mixed
+     */
+    public function getAll($relations = [])
+    {
+        return $this->model->with($relations)->all();
+    }
+
+    /**
+     * @param array $relations
+     * @param int $perPage
+     * @return mixed
+     */
+    public function getPaginated($relations = [], $perPage = 15)
+    {
+        return $this->model->with($relations)->paginate($perPage);
+    }
+}
---
+++ app/Repositories/Status/StatusRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories\Status;
+
+interface StatusRepository
+{
+    /**
+     * @return mixed
+     */
+    public function getAll();
+
+    /**
+     * @param int $id
+     * @return mixed
+     */
+    public function getById(int $id);
+
+    /**
+     * @param array $relations
+     * @param int $perPage
+     * @return mixed
+     */
+    public function getPaginated($relations =[], $perPage = 15);
+}
