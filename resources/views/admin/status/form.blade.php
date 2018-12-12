d7d518bff0203c22a8fe3b6d5eedd2562a9e9775
AUzhegov <test>
2018-12-12 17:27:28 +0300

    Status minor

--- resources/views/admin/status/form.blade.php
+++ resources/views/admin/status/form.blade.php
@@ -15,12 +15,13 @@
         {{ Form::label('color', 'Цвет бейджа', ['class' => 'col-sm-2 control-label']) }}
         <div class="col-sm-10">
             {{ Form::select('color', ['default' => 'Серый (default)', 'primary' => 'Синий (primary)', 'success' => 'Зеленый (success)', 'info' => 'Голубой (info)', 'warning' => 'Жёлтый (warning)', 'danger' => 'Красный'], null, array('class' => 'form-control')) }}
-            <span class="label label-default">Default Label</span>
-            <span class="label label-primary">Primary Label</span>
-            <span class="label label-success">Success Label</span>
-            <span class="label label-info">Info Label</span>
-            <span class="label label-warning">Warning Label</span>
-            <span class="label label-danger">Danger Label</span>
+            <small>Возможные цвета:</small><br>
+            <span class="label label-default">Default</span>
+            <span class="label label-primary">Primary</span>
+            <span class="label label-success">Success</span>
+            <span class="label label-info">Info</span>
+            <span class="label label-warning">Warning</span>
+            <span class="label label-danger">Danger</span>
 
         </div>
     </div>
