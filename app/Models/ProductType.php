f2e84ac50d57f554b9138b5e0bbf954a55496a58
AUzhegov <test>
2018-12-25 15:33:46 +0300

    Comm

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -14,11 +14,19 @@ class ProductType extends Model
         'name'
     ];
 
+    /**
+     * Product relation
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function product()
     {
         return $this->hasMany(Product::class);
     }
 
+    /**
+     * Product properties relation
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function productProps()
     {
         return $this->belongsTo(Props::class);
