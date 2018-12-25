ac746c9bb4ed3366d5686838ef77ea0302fed08a
AUzhegov <test>
2018-12-25 15:33:33 +0300

    Array minor changes

--- resources/views/admin/product/form.blade.php
+++ resources/views/admin/product/form.blade.php
@@ -14,7 +14,7 @@
     <div class="form-group">
         {{ Form::label('disable', 'Активность', ['class' => 'col-sm-2 control-label']) }}
         <div class="col-sm-10">
-            {{ Form::select('disable', ['Активно', 'Неактивно'], null, array('class' => 'form-control')) }}
+            {{ Form::select('disable', ['Активно', 'Неактивно'], null, ['class' => 'form-control']) }}
             <small>В зависимости от выбранной активности, продукт будет или не будет отображаться в каталоге</small>
         </div>
     </div>
@@ -28,14 +28,14 @@
     <div class="form-group">
         {{ Form::label('categories[]', 'Категории', ['class' => 'col-sm-2 control-label']) }}
         <div class="col-sm-10">
-        {{ Form::select('categories[]', $categories->pluck('name', 'id'), null, array('class' => 'form-control', 'multiple', 'required')) }}
+        {{ Form::select('categories[]', $categories->pluck('name', 'id'), null, ['class' => 'form-control', 'multiple', 'required']) }}
          <small>Мультивыбор по CTRL</small>
         </div>
     </div>
     <div class="form-group">
         {{ Form::label('brand_id', 'Бренд', ['class' => 'col-sm-2 control-label']) }}
         <div class="col-sm-10">
-            {{ Form::select('brand_id', $brand->pluck('name', 'id'), null, array('class' => 'form-control', 'required')) }}
+            {{ Form::select('brand_id', $brand->pluck('name', 'id'), null, ['class' => 'form-control', 'required']) }}
         </div>
     </div>
     <div class="form-group">
@@ -55,6 +55,6 @@
     <div class="form-group">
         {{ Form::label('type_id', 'Тип продукта', ['class' => 'col-sm-2 control-label']) }}
         <div class="col-sm-10">
-            {{ Form::select('type_id', $type->pluck('name', 'id'), null, array('class' => 'form-control type_selector', 'required')) }}
+            {{ Form::select('type_id', $type->pluck('name', 'id'), null, ['class' => 'form-control type_selector', 'required']) }}
         </div>
     </div>
