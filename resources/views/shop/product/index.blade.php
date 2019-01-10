cdb0cdaca0359d539b39e06d4c65765852f9a2dc
AUzhegov <test>
2019-01-10 12:11:52 +0300

    Add tabs to product page

--- resources/views/shop/product/index.blade.php
+++ resources/views/shop/product/index.blade.php
@@ -36,31 +36,8 @@
                         {{ $product->name }}
                     </h4>
                     <h5>Стоимость: {{ $product->price }} <i class="fas fa-dollar-sign"></i></h5>
-                    <hr>
-                    <p class="card-text">{{ $product->description }}</p>
-                </div>
-                <nav>
-                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                        <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
-                        <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
-                        <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
-                    </div>
-                </nav>
-                <div class="tab-content" id="nav-tabContent">
-                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                        <table class="table table-hover">
-                            <tbody>
-                            @foreach($product->props as $prop)
-                                <tr>
-                                    <td><b>{{ $prop->name }}</b></td>
-                                    <td>{{ $prop->pivot->value }}</td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
-                    <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+                    <br>
+                    @include('shop.product.product_tabs')
                 </div>
                 <div class="card-footer">
                     <div class="detail-product-cart__container">
---
+++ resources/views/shop/product/product_tabs.blade.php
@@ -0,0 +1,44 @@
+<nav>
+    <div class="nav nav-pills" id="nav-tab" role="tablist">
+        <a class="nav-item nav-link active" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab" aria-controls="nav-about" aria-selected="true">Описание</a>
+        <a class="nav-item nav-link" id="nav-props-tab" data-toggle="tab" href="#nav-props" role="tab" aria-controls="nav-props" aria-selected="false">Характеристики</a>
+        <a class="nav-item nav-link" id="nav-feedback-tab" data-toggle="tab" href="#nav-feedback" role="tab" aria-controls="nav-feedback" aria-selected="false">Отзывы</a>
+        <a class="nav-item nav-link" id="nav-topic-tab" data-toggle="tab" href="#nav-topic" role="tab" aria-controls="nav-topic" aria-selected="false">Обсуждение</a>
+
+    </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+    <br>
+    <!-- ABOUT TAB -->
+
+    <div class="tab-pane fade show active" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
+        <p class="card-text">{{ $product->description }}</p>
+    </div>
+
+    <!-- PROPS TAB -->
+
+    <div class="tab-pane fade" id="nav-props" role="tabpanel" aria-labelledby="nav-props-tab">
+        <table class="table table-borderless">
+            <tbody>
+            @foreach($product->props as $prop)
+                <tr>
+                    <td><b>{{ $prop->name }}</b></td>
+                    <td>{{ $prop->pivot->value }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <!-- FEEDBACK TAB -->
+
+    <div class="tab-pane fade" id="nav-feedback" role="tabpanel" aria-labelledby="nav-feedback-tab">
+        in dev...
+    </div>
+
+    <!-- TOPIC TAB -->
+
+    <div class="tab-pane fade" id="nav-topic" role="tabpanel" aria-labelledby="nav-topic-tab">
+        in dev...
+    </div>
+</div>
