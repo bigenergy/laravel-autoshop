2684e7b676afa47deef397e450b46ffbfb449c2a
AUzhegov <test>
2019-01-11 11:22:37 +0300

    Restyle categories

--- resources/views/shop/layouts/categories.blade.php
+++ resources/views/shop/layouts/categories.blade.php
@@ -1,7 +1,6 @@
-<h6>Категории</h6>
 <div class="list-group">
 @forelse($categoryShowList as $category)
-    <a href="/categories/{{ $category->slug }}" class="list-group-item {{ set_active(["categories/{$category->slug}"]) }}"><i class="fas fa-car"></i> {{ $category->name }}</a>
+    <a href="/categories/{{ $category->slug }}" class="dropdown-item {{ set_active(["categories/{$category->slug}"]) }}"><i class="fas fa-car"></i> {{ $category->name }}</a>
 @empty
     <a href="#" class="list-group-item">Нет категорий</a>
 @endforelse
