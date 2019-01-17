d11c409604c6ac481af6e0a8e23957117dd2c9a8
AUzhegov <test>
2019-01-17 18:33:44 +0300

    Restyle filter

--- resources/views/shop/filter/filter.blade.php
+++ resources/views/shop/filter/filter.blade.php
@@ -18,63 +18,78 @@
             <a href="#" class="list-group-item">Нет типов товаров</a>
         @endforelse
     </div>
-    <h5><i class="fas fa-dollar-sign"></i> Цена</h5>
-    <div class="form-row">
-        <div class="form-group col-md-6">
-            <label>От</label>
-            <input type="number"
-                   class="form-control"
-                   name="min_price"
-                   min="{{ $products->min('price') }}"
-                   max="{{ $products->max('price') }}"
-                   id="min_price" {{ !empty(Request::get('min_price')) ? "placeholder=".  Request::get('min_price'): "value=" . $products->min('price')}}>
+        <div class="card" >
+            <div class="card-header">
+               <i class="fas fa-dollar-sign"></i> Цена
+            </div>
+            <div class="form-row container mb-3 mt-3 ">
+                <div class="input-group ">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">От</span>
+                    </div>
+                    <input type="number"
+                           class="form-control"
+                           name="min_price"
+                           min="{{ $products->min('price') }}"
+                           max="{{ $products->max('price') }}"
+                           id="min_price" {{ !empty(Request::get('min_price')) ? "placeholder=".  Request::get('min_price'): "value=" . $products->min('price')}}>
+                    <div class="input-group-prepend">
+                        <span class="input-group-text">До</span>
+                    </div>
+                    <input type="number"
+                           class="form-control"
+                           name="max_price"
+                           min="{{ $products->min('price') }}"
+                           max="{{ $products->max('price') }}"
+                           id="max_price" {{ !empty(Request::get('max_price')) ? "placeholder=".  Request::get('max_price'): "value=" . $products->max('price')}}>
+                </div>
+            </div>
+
+            <div class="list-group list-group-flush">
+                <button type="button" class="btn btn-outline-primary btn-sm list-group-item text-center"><i class="fas fa-redo"></i> Сбросить фильтры</button>
+            </div>
+
+
+            <div class="card-header">
+                <i class="fas fa-list-ol"></i> Категории
+            </div>
+            <div class="list-group list-group-flush">
+                @forelse($categories as $category)
+                    <label class="form-check-label list-group-item list-group-item-action">
+                        <input type="checkbox"
+                               name="categories[]"
+                               value="{{ $category->id }}"
+                                {{ !empty(Request::get('categories')) && in_array($category->id, Request::get('categories')) ? "checked" : ""}}
+                        >
+                        {{ $category->name }}
+                    </label>
+                @empty
+                    <a href="#" class="list-group-item">Нет категорий</a>
+                @endforelse
+            </div>
+
+            <div class="card-header">
+                <i class="fab fa-mailchimp"></i> Производитель
+            </div>
+            <div class="list-group list-group-flush">
+                @forelse($brands as $brand)
+                    <label class="form-check-label list-group-item list-group-item-action">
+                        <input type="checkbox" name="brands[]"
+                               class="filter_brand_{{ $brand->id }}"
+                               value="{{ $brand->id }}"
+                                {{ !empty(Request::get('brands')) && in_array($brand->id, Request::get('brands')) ? "checked" : ""}}
+                        >
+                        {{ $brand->name }}
+                    </label>
+                @empty
+                    <a href="#" class="list-group-item">Нет производителей</a>
+                @endforelse
+            </div>
         </div>
-        <div class="form-group col-md-6 text-right">
-            <label>До</label>
-            <input type="number"
-                   class="form-control"
-                   name="max_price"
-                   min="{{ $products->min('price') }}"
-                   max="{{ $products->max('price') }}"
-                   id="max_price" {{ !empty(Request::get('max_price')) ? "placeholder=".  Request::get('max_price'): "value=" . $products->max('price')}}>
-        </div>
-    </div>
-    <h5><i class="fas fa-list-ol"></i> Категории</h5>
-    <div class="list-group mb-4">
-        <div class="">
-            @forelse($categories as $category)
-                <label class="form-check-label list-group-item list-group-item-action">
-                    <input type="checkbox"
-                           name="categories[]"
-                           value="{{ $category->id }}"
-                           {{ !empty(Request::get('categories')) && in_array($category->id, Request::get('categories')) ? "checked" : ""}}
-                    >
-                    {{ $category->name }}
-                </label>
-            @empty
-                <a href="#" class="list-group-item">Нет категорий</a>
-            @endforelse
-        </div>
-    </div>
-    <h5><i class="fab fa-mailchimp"></i> Производитель</h5>
-    <div class="list-group mb-4">
-        <div class="">
-        @forelse($brands as $brand)
-            <label class="form-check-label list-group-item list-group-item-action">
-                <input type="checkbox" name="brands[]"
-                       class="filter_brand_{{ $brand->id }}"
-                       value="{{ $brand->id }}"
-                        {{ !empty(Request::get('brands')) && in_array($brand->id, Request::get('brands')) ? "checked" : ""}}
-                >
-                {{ $brand->name }}
-            </label>
-        @empty
-            <a href="#" class="list-group-item">Нет производителей</a>
-        @endforelse
-        </div>
-    </div>
+
+
     <input type="hidden" name="slug" id="slug" value="{{ $catalogType->slug }}">
     <input type="hidden" name="sort" id="sort" value="name">
-    <input type="hidden"  name="sort_type" id="sort_type" value="asc">
+    <input type="hidden" name="sort_type" id="sort_type" value="asc">
     </fieldset>
 </form>
