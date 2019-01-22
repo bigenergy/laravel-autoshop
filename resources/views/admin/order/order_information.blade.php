097551feb50d92cd8e465bdd8432e493f2287b36
AUzhegov <test>
2019-01-22 17:46:32 +0300

    Add created & updated_at in order detail

--- resources/views/admin/order/order_information.blade.php
+++ resources/views/admin/order/order_information.blade.php
@@ -2,3 +2,7 @@
 <div class="col-md-6"><b id = "orderItem-quantity">{{ $order->productsCount ?? 0 }} ед.</b></div>
 <div class="col-md-6"><b>Стоимость заказа:</b></div>
 <div class="col-md-6"><b id = "orderItem-total_price">{{ $order->productsPrice ?? 0 }} $</b></div>
+<div class="col-md-6"><b>Дата заказа:</b></div>
+<div class="col-md-6"><b id = "orderItem-total_price">{{ $order->created_at }}</b></div>
+<div class="col-md-6"><b>Дата изменения:</b></div>
+<div class="col-md-6"><b id = "orderItem-total_price">{{ $order->updated_at }}</b></div>
