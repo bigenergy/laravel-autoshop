3fd12dea9157df5d8c0abdb5925ea00172191744
unknown <andre90094@gmail.com>
2019-05-20 02:38:06 +0300

    Fix dropdown menu categories

--- resources/views/shop/layouts/categories.blade.php
+++ resources/views/shop/layouts/categories.blade.php
@@ -2,6 +2,6 @@
 @forelse($productTypes as $type)
     <a href="/catalog/{{ $type->slug }}" class="dropdown-item {{ set_active(["catalog/{$type->slug}"]) }}"><i class="fas fa-car"></i> {{ $type->name }}</a>
 @empty
-    <a href="#" class="list-group-item">Нет категорий</a>
+    <a href="#" class="dropdown-item">Нет категорий</a>
 @endforelse
 </div>
